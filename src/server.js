a21471f8c10cdea2a65ef5e5da03ffe99336e81a
mauriciobranda <mauricio.20@gmail.com>
2021-10-24 23:32:21 -0300

    AJUSTE

--- src/server.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var app_1 = require("./app");
-require("reflect-metadata");
-require("./database");
-app_1["default"].listen(3000, function () {
-    console.log('🏃 Running Server');
-});
