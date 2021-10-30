b565d6447128b2287a5e3edce5a730418c55a771
mauriciobranda <mauricio.20@gmail.com>
2021-10-30 16:53:36 -0300

    [Update] Alterado Student Models para class validation

--- src/models/Student.ts
+++ src/models/Student.ts
@@ -8,7 +8,7 @@ import {
     JoinTable,
   } from 'typeorm';
 import Class from './Class';
-import {IsEmail} from 'class-validator';
+import {IsEmail, Max, MaxLength, Min} from 'class-validator';
 
 @Entity('student')
 export class Student {
@@ -26,6 +26,9 @@ export class Student {
     length: 45,
     unique: true,
     })
+    //@MaxLength(6)
+    @Max(9999)
+    @Min(1)
     key: string;
 
     @Column({
